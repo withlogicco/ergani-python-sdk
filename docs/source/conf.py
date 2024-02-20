44e8d99fb82fccc5a5713fd8ed0019eefead49d8
George Margaritis <george@withlogic.co>
2024-02-20 18:06:04 +0200

    Bump version to `1.0.1` in docs (#15)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "ergani"
 copyright = "MIT"
 author = "George Margaritis <george@withlogic.co>"
-release = "1.0.0"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
